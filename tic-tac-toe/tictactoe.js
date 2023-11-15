f6d69aa60ab97eea0ab87c07bf6cfaf7370bba4f
Elton <28992495+Lucrum@users.noreply.github.com>
2023-11-15 13:50:38 -0500

    Board manipulation

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -1,13 +1,24 @@
 const gameboard = (function () {
-  let board = [[null, null, null], [null, null, null], [null, null, null]]
+  let board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
   const addPiece = (x, y, player) => {
-    if (board[x][y] == null) { // allow for falsey values to also stand in place of null
-      board[x][y] = player.piece
+    if (board[x][y] == false) { // falsy values indicate empty space
+      board[x][y] = player.token
     }
   }
+
+  const displayBoard = () => {
+    console.log(`
+       ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
+      ---+---+---
+       ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
+      ---+---+---
+       ${board[2][0]} | ${board[2][1]} | ${board[2][2]}
+    `)
+  }
+
+  return { addPiece, displayBoard }
 })()
 
 function createPlayer (name, token) {
   return { name, token }
 }
-
